7eeaffd4b2515dc75f68c2d857cc963021473eab
tetsuyahirose <tetsuya19990510gmail.com>
2020-06-28 17:25:51 +0900

    no message

--- CircleImage.swift
+++ CircleImage.swift
@@ -17,8 +17,3 @@ struct CircleImage: View {
     }
 }
 
-//struct CircleImage_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CircleImage(image: Image("turtlerock"))
-//    }
-//}
